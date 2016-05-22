55593b57362be603799fdac7b300417619e42296
Phillip Ramirez <ramirezp6856@gmail.com>
2016-05-21 22:23:39 -0400

    Create app-modifythis.js
    Different for each environment in terms of database and client or localhost.

---
+++ app-modifythis.js
@@ -0,0 +1,54 @@
+var express = require('express');
+
+var sql = require('mssql');
+
+// Config for SQLEXPRESS
+var config = {
+    user: '',
+    password: '',
+    server: '',
+    database: 'Books',
+    options: {
+       instance: 'SQLEXPRESS'
+    }
+};
+
+sql.connect(config, function(err){
+    console.log(err);
+});
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+var nav = [{
+        Link: '/Books',
+        Text: 'Books'
+            }, {
+        Link: '/Authors',
+        Text: 'Authors'
+}];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+
+
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: nav
+    });
+});
+
+app.get('/books', function (req, res) {
+    res.send('Hello Books!');
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+});
